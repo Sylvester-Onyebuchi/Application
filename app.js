e49b2b1abdb97a92674d5b7f4707f52fe0c54780
Sylvester-Onyebuchi <onahsylvester3@gmail.com>
2024-10-19 11:55:46 +0200

    modified app.js

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ const flash = require('express-flash')
 const mongoose = require('mongoose')
 const path = require('path')
 const passport = require('./middleware/passport')
-// const mongoStore = require('connect-mongo')
+const mongoStore = require('connect-mongo')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
-mongoose.connect(process.env.MONGO_DB)
+mongoose.connect(process.env.MONGO_DB || "mongodb://localhost:27017/TODO")
 .then(() => console.log("Database connected successfully"))
 .catch(err => {console.log(err)})
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(session({
-    secret:process.env.SECRET,
+    secret:process.env.SECRET || 'Todo-App',
+    store: mongoStore.create({mongoUrl:'mongodb://localhost:27017/TODO', collectionName:'session'}),
     resave: false,
     saveUninitialized: false,
+    cookie: {maxAge: 2 * 24 * 60 * 60 * 1000}
 }))
 app.use(express.urlencoded({extended:true}))
 app.use(passport.initialize())
