5cd0bfc9696cf9b2f34cd5fe60694b85bd0835c1
Sylvester-Onyebuchi <onahsylvester3@gmail.com>
2024-10-19 11:34:20 +0200

    modified app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const flash = require('express-flash')
 const mongoose = require('mongoose')
 const path = require('path')
 const passport = require('./middleware/passport')
-const mongoStore = require('connect-mongo')
+// const mongoStore = require('connect-mongo')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -15,10 +15,8 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(session({
     secret:process.env.SECRET,
-    store: mongoStore.create({mongoUrl:'mongodb://localhost:27017/TODO', collectionName:'session'}),
     resave: false,
     saveUninitialized: false,
-    cookie: {maxAge: 2 * 24 * 60 * 60 * 1000}
 }))
 app.use(express.urlencoded({extended:true}))
 app.use(passport.initialize())
